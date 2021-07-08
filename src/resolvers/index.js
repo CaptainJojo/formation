e3ff7efd7c1bf3a20703d6e07ddd4e01657833b9
Antoine <alepiller@bimandco.com>
2021-07-08 11:51:13 +0200

    add resolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,9 +1,11 @@
 import books from './query/books';
 import author from './book/author';
+import bimObjects from './query/bimObjects';
 
 export default {
   Query: {
     books,
+    bimObjects,
   },
   Book: {
     author,
