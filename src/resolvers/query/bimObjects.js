67624e84181348c831ddcc7f9c57a6108893f58d
Antoine <alepiller@bimandco.com>
2021-07-08 14:17:59 +0200

    add join

--- src/resolvers/query/bimObjects.js
+++ src/resolvers/query/bimObjects.js
@@ -1,6 +1,9 @@
 import knexConnection from '../../config/db';
 
 export default async (_, { filter }) => {
-    const results = await knexConnection.select().from('bimobjects').limit(2);
+    const results = await knexConnection
+    .select()
+    .from('bimobjects')
+    .limit(2);
     return results;
 }
