814192fb783c365294fc8beafd58769c6a1581cc
Antoine <alepiller@bimandco.com>
2021-07-09 10:06:29 +0200

    add pagination bimobjects

--- src/dataSources/bimobject-datasource.js
+++ src/dataSources/bimobject-datasource.js
@@ -2,13 +2,14 @@ import PSQLDataSource from './psql-data-source';
 
 export default class BimobjectDataSource extends PSQLDataSource{
 
-async GetBimObjects(){
+async GetBimObjects(first, after){
     const results = await this.knexConnection
-    .select()
+    .select('id')
     .from('bimobjects')
-    .limit(10);
+    .where('id','>',after ?? 0)
+    .orderBy('id', 'desc')
+    .limit(first+1);
     return results;
 }
 
 }
-
--- src/resolvers/query/bimObjects.js
+++ src/resolvers/query/bimObjects.js
@@ -1,3 +1,15 @@
-export default async (_, { filter }, context) => {
-    return context.dataSources.bimobject.GetBimObjects();
+export default async (_, { filter, first, after }, context) => {
+    const results = await context.dataSources.bimobject.GetBimObjects(first, after);
+    const resultsToSend = results.slice(0, first); 
+   
+    return {
+        edges: resultsToSend.map(result => ({
+            node: result,
+            cursor: result.id
+        })),
+        pageInfo: {
+            hasNextPage: results.length === (first+1),
+            endCursor: results[resultsToSend.length-1].id
+        }
+    }
 }
